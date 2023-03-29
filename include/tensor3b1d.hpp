26c81ef12224d46ecb2a6b6edf4c10960b96d0c7
Zhaonan Meng <zhaonanmeng@login02.delftblue.tudelft.nl>
2023-03-29 18:01:02 +0200

    add apply operator

--- include/tensor3b1d.hpp
+++ include/tensor3b1d.hpp
@@ -6,14 +6,34 @@ namespace QRes
   template<typename ST>
   class Kron2D{
     public:
-      Kron2D(int m, ST* C_n, 
-	     int n, ST* D_m,
+      Kron2D(int m, ST* C_m, 
+	     int n, ST* D_n,
 	     ST* V, ST a1, ST a2):
-             C_(C_n), D_(D_m), V_(V), a1_(a1), a2_(a2), n_(n), m_(m){};
+             C_(C_m), D_(D_n), V_(V), a1_(a1), a2_(a2), n_(n), m_(m){};
 
-      void apply() // to do...
-      { std::cout << "tensor apply operator succeed!\n" << std::endl;
-	      return;}
+      // reshape operator
+      // y = Op*x = reshape(a1*C*X+a2*X*D', n*m,1)
+      void apply(const ST* v_in, ST* v_out) // to do...
+      {
+
+	int i,j,k; 
+	// V*w 
+	for (i=0; i<m_*n_; i++) v_out[i] = V_[i] * v_in[i]; 
+	
+	// a2*C2*W
+	for (i=0; i<m_; i++) 
+          for (j=0; j<n_; j++) 
+            for (k=0; k<n_; k++) 
+	      v_out[i*n_+j] += a2_ * D_[j*n_+k] * v_in[i*n_+k];
+       
+        // a1*W*C1^T
+	for (i=0; i<m_; i++)
+          for (j=0; j<n_; j++)
+	    for (k=0; k<m_; k++)
+	      v_out[i*n_+j] += a1_ * v_in[k*n_+j] * C_[i*m_+k];   
+
+        return;
+      }
 
     private:
       int n_, m_;
