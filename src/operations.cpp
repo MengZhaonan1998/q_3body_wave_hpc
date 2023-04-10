5c5012cb3f74fd559aaccc4dd114fa00c925a094
Zhaonan Meng <zhaonanmeng@login01.delftblue.tudelft.nl>
2023-04-10 11:21:31 +0200

    add complex vector operations

--- src/operations.cpp
+++ src/operations.cpp
@@ -1,5 +1,5 @@
 #include "operations.hpp"
-#include <omp.h>
+#include "header.hpp"
 
 void init(int n, double* x, double value)
 {
@@ -11,6 +11,7 @@ void init(int n, double* x, double value)
   return;
 }
 
+
 double dot(int n, double const* x, double const* y)
 {
   double res=0.0;
@@ -19,9 +20,21 @@ double dot(int n, double const* x, double const* y)
   for (int i=0; i<n; i++)
      res += x[i]*y[i];
   
-  return res;
-  
+  return res;  
 }
+
+
+std::complex<double> complex_dot(int n, int startx, int starty, std::complex<double> const* x, std::complex<double> const* y)
+{
+  std::complex<double> res(0.0,0.0);
+  
+ // #pragma omp parallel for reduction(+:res)
+  for (int i=0; i<n; i++)
+     res += std::conj(x[i+startx]) * y[i+starty];
+
+  return res;
+}
+
 
 void axpby(int n, double a, double const* x, double b, double* y)
 {
@@ -31,74 +44,18 @@ void axpby(int n, double a, double const* x, double b, double* y)
   return;
 }
 
-//! apply a 7-point stencil to a vector
-void apply_stencil3d(stencil3d const* S,
-        double const* u, double* v)
+
+void complex_axpby(int n, int startx, int starty, std::complex<double> a, std::complex<double>* x, std::complex<double> b, std::complex<double>* y)
 {
-  int nx=S->nx, ny=S->ny, nz=S->nz;  // access the number of points in x, y, z coordinate
-  double ele;                        // I use ele to store the value of v[S->index_c(i,j,k)]
-/* 
-  #pragma omp parallel for reduction(+:ele)
-  for (int k=0; k<nz; k++)
-    for (int j=0; j<ny; j++)
-        for(int i=0; i<nx; i++)
-	{
-	  ele = S->value_c * u[S->index_c(i,j,k)] +
-	        S->value_e * u[S->index_e( i-(i==(nx-1)), j, k)] * (i < nx - 1) +
-		S->value_w * u[S->index_w( i+(i==0), j, k)] * (i > 0)  +
-		S->value_n * u[S->index_n( i, j-(j==(ny-1)), k)] * (j < ny - 1) +
-		S->value_s * u[S->index_s( i, j+(j==0), k)] * (j > 0)  +
-	        S->value_t * u[S->index_t( i, j, k-(k==(nz-1)))] * (k < nz - 1) +
-		S->value_b * u[S->index_b( i, j, k+(k==0))] * (k > 0);	 
-	  v[S->index_c(i,j,k)] = ele;  
-	}
-  
-  return;
-}*/
-
-  #pragma omp parallel for reduction(+:ele)
-  for (int k=0; k<nz; k++) 
-     for (int j=0; j<ny; j++)
-        for(int i=0; i<nx; i++) 
-	{
-	   ele  = S->value_c * u[S->index_c(i,j,k)];
-
-	   // If the point is the first or last point on the x coordinate, we don't
-	   // consider the east or west point as a stencil point since it's the boundary
-	   // point included by the righ hand side array b 
-           if (i==0||i==nx-1)
-	      ele += (i==0) ? 
-	         (u[S->index_e(i,j,k)] * S->value_e): 
-		 (u[S->index_w(i,j,k)] * S->value_w); 
-	   else 
-	      ele += u[S->index_e(i,j,k)]*S->value_e + 
-			    u[S->index_w(i,j,k)]*S->value_w; 	
-           
-	   // If the point is the first or last point on the y coordinate, we don't
-	   // consider the south or north point as a stencil point since it's the boundary
-	   // point included by the righ hand side array b 
-           if (j==0||j==ny-1) 
-	      ele += (j==0) ? 
-	         (u[S->index_n(i,j,k)] * S->value_n): 
-		 (u[S->index_s(i,j,k)] * S->value_s);
-	   else 
-	      ele += u[S->index_n(i,j,k)]*S->value_n + 
-			    u[S->index_s(i,j,k)]*S->value_s; 	
-
-	   
-	   // If the point is the first or last point on the z coordinate, we don't
-	   // consider the bottom or top point as a stencil point since it's the boundary
-	   // point included by the righ hand side array b 
-           if (k==0||k==nz-1) 
-	      ele += (k==0) ? 
-	         (u[S->index_t(i,j,k)] * S->value_t): 
-		 (u[S->index_b(i,j,k)] * S->value_b);  
-	   else 
-	      ele += u[S->index_t(i,j,k)]*S->value_t + 
-		            u[S->index_b(i,j,k)]*S->value_b; 	
-
-	   v[S->index_c(i,j,k)] = ele;   // assign the value of ele to the array v
-	}
+  for (int i=0; i<n; i++)
+     y[i+starty] = (a * x[i+startx] + b * y[i+starty]);
   return;
 }
 
+
+void complex_0xpby(int n, int start, std::complex<double> b, std::complex<double>* y)
+{
+  for (int i=start; i<n+start; i++)
+     y[i] = b * y[i];
+  return;
+}
