5ec961c57eee955ef64e5ab476b3aec1b0618cc8
Zhaonan Meng <zhaonanmeng@login02.delftblue.tudelft.nl>
2023-04-30 12:38:54 +0200

    more comments are added

--- src/QRes_3b1d.cpp
+++ src/QRes_3b1d.cpp
@@ -3,14 +3,16 @@
 #include "timer.hpp"
 #include "tensor3b1d.hpp"
 
-std::map<std::string, std::string> readJDopts()
+/* readopts()
+ * used to read options for the JacobiDavidson algorithm and GMRES solver */
+std::map<std::string, std::string> readopts(std::string filename)
 {
-  std::map<std::string, std::string> jdopts;
+  std::map<std::string, std::string> opts;
 
   std::string line;
   std::vector<std::string> v;
   std::ifstream fin;
-  fin.open("jacobidavidsonOpts.txt");
+  fin.open(filename);
 
   if (fin.is_open())
   {
@@ -25,9 +27,9 @@ std::map<std::string, std::string> readJDopts()
   }
 
   for (int i=0; i<v.size(); i++) 
-    if (i%2==0) jdopts[v[i]] = v[i+1];
+    if (i%2==0) opts[v[i]] = v[i+1];
    
-  return jdopts;
+  return opts;
 }	
 
 
@@ -48,22 +50,35 @@ int main(int argc, char* argv[])
 
   if (proc_rank==0)
   std::cout << "Welcome to QRes_3b1d!\n" 
+	    << "              -----------                      \n"
+	    << "---------     \\        /              ------- \n"
+	    << "\\      /-------\\      /---------------\\    / \n"             
+            << " \\    /---------\\    /-----------------\\  /  \n"
+	    << "  \\  /           \\  /                   \\/   \n"
+	    << "   \\/             \\/                          \n"
 	    << "This program aims at computing resonances of a quantum three body problem in parallel.\n" << std::endl;
  
   int nR = atoi(argv[1]);    // x grid size
   int nr = atoi(argv[2]);    // y grid size
-  if (nR <= 2 || nr <= 2) throw std::runtime_error("need at least two grid points in each dimension to implement boundary conditions");
   double LR = atoi(argv[3]); // x domain length
   double Lr = atoi(argv[4]); // y domain length
+  if (nR <= 2 || nr <= 2) throw std::runtime_error("need at least two grid points in each dimension to implement boundary conditions");
 
   //--- read options of Jacobi-Davidson algorithm ---//
-  auto jdopts = readJDopts(); 
-  std::cout << "Options of the JD algorithm are as follows:" << std::endl;
+  auto jdopts = readopts("jacobidavidsonOpts.txt"); 
+  std::cout << "Options of the Jacobi-Davidson algorithm are as follows:" << std::endl;
   for (const auto& [key, value] : jdopts)
 	  std::cout<< key << "=" << value << std::endl;
 
+  //--- read options of GMRES algorithm ---//
+  auto gmresopts = readopts("gmresOpts.txt"); 
+  std::cout << "Options of the GMRES algorithm are as follows:" << std::endl;
+  for (const auto& [key, value] : gmresopts)
+	  std::cout<< key << "=" << value << std::endl;
+
   //--- Jacobi-Davidson eigensolver ---//
-  auto result = JacobiDavidson(nR, nr, LR, Lr, jdopts);    // quadratic jacobi-davidson algorithm
+  auto result = JacobiDavidson(nR, nr, LR, Lr, jdopts, gmresopts);    // quadratic jacobi-davidson algorithm
+  
   MPI_Finalize();                          // finish mpi
   
   //--- display the result including eigenvalues... ---//
