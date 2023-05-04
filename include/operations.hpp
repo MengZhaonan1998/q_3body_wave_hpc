f90e4b52fc0ecd98d5cf74692024e03beea4d9d0
Zhaonan Meng <zhaonanmeng@login04.delftblue.tudelft.nl>
2023-05-04 12:36:51 +0200

    overload functions axpby dot init to enable them support double and complex at the same time

--- include/operations.hpp
+++ include/operations.hpp
@@ -11,7 +11,7 @@
 void init(int n, double* x, double value);
 
 // initialize a complex vector with a complex number, x[i] = value for 0<=i<n
-void complex_init(int n, std::complex<double>* x, std::complex<double> value);
+void init(int n, std::complex<double>* x, std::complex<double> value);
 
 // scalar product: return sum_i x[i]*y[i] for 0<=i<n
 std::complex<double> dot(int n, std::complex<double> const* x, std::complex<double> const* y);
@@ -23,9 +23,11 @@ std::complex<double> complex_dot(int n, std::complex<double> const* x, std::comp
 void axpby(int n, double a, double const* x, double b, double* y);
 
 // complex vector update: compute y[i] = a*x[i] + b*y[i] for 0<=i<n
-void complex_axpby(int n, std::complex<double> a, std::complex<double> const* x, std::complex<double> b, std::complex<double>* y);
+void axpby(int n, std::complex<double> a, std::complex<double> const* x, std::complex<double> b, std::complex<double>* y);
 
 // complex vector update: compute y[i] = a*x[i] for 0<=i<n
 void vec_update(int n, std::complex<double> a, std::complex<double> const* x, std::complex<double>* y);
 
+// domain decomposition
+int domain_decomp(int global);
 
--- src/operations.cpp
+++ src/operations.cpp
@@ -13,9 +13,8 @@ void init(int n, double* x, double value)
 }
 
 
-void complex_init(int n, std::complex<double>* x, std::complex<double> value)
+void init(int n, std::complex<double>* x, std::complex<double> value)
 {
- 
   #pragma omp parallel for 
   for (int i=0; i<n; i++)
      x[i] = value;   // assign value to every entry of x
@@ -26,46 +25,34 @@ void complex_init(int n, std::complex<double>* x, std::complex<double> value)
 
 std::complex<double> dot(int n, std::complex<double> const* x, std::complex<double> const* y)
 {
-  std::complex<double> res=0.0;
-  
+  std::complex<double> local_res=0.0;
+  std::complex<double> global_res;   
   /* It's already 2023, and OpenMP still doesn't support complex number operations?! 
    * No... We can define the reduction using 'declare' */
   #pragma omp declare reduction(+: std::complex<double>: omp_out += omp_in) initializer (omp_priv = omp_orig) 
-  #pragma omp parallel for reduction(+:res) 
+  #pragma omp parallel for reduction(+:local_res) 
   for (int i=0; i<n; i++)
-     res += x[i]*y[i];
+     local_res += x[i]*y[i];
   
-  return res;  
+  MPI_Allreduce(&local_res, &global_res, 1, MPI_DOUBLE_COMPLEX, MPI_SUM, MPI_COMM_WORLD);
+  
+  return global_res;  
 }
 
 
 std::complex<double> complex_dot(int n, std::complex<double> const* x, std::complex<double> const* y)
 {
-  std::complex<double> res(0.0,0.0);
-  
+  std::complex<double> local_res(0.0,0.0);
+  std::complex<double> global_res;
   #pragma omp declare reduction(+: std::complex<double>: omp_out += omp_in) initializer (omp_priv = omp_orig)
-  #pragma omp parallel for reduction(+:res)
+  #pragma omp parallel for reduction(+:local_res)
   for (int i=0; i<n; i++)
-     res += std::conj(x[i]) * y[i];
+     local_res += std::conj(x[i]) * y[i];
+  
+  MPI_Allreduce(&local_res, &global_res, 1, MPI_DOUBLE_COMPLEX, MPI_SUM, MPI_COMM_WORLD);
 
-  return res;
+  return global_res;
 }
-
-/*
-double dot(int const local_n, double const* x, double const* y) {
-double local_dot = 0.0;
-
-int size;
-MPI_Comm_size(MPI_COMM_WORLD, &size);
-for (int id = 0; id < local_n; id++) {
-	local_dot += x[id] * y[id];
-}
-
-double global_dot;
-MPI_Allreduce(&local_dot, &global_dot, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-
-return global_dot;
-}*/
 
 
 void axpby(int n, double a, double const* x, double b, double* y)
@@ -77,7 +64,7 @@ void axpby(int n, double a, double const* x, double b, double* y)
 }
 
 
-void complex_axpby(int n, std::complex<double> a, std::complex<double> const* x, std::complex<double> b, std::complex<double>* y)
+void axpby(int n, std::complex<double> a, std::complex<double> const* x, std::complex<double> b, std::complex<double>* y)
 {
   #pragma omp parallel for
   for (int i=0; i<n; i++)
@@ -92,5 +79,29 @@ void vec_update(int n, std::complex<double> a, std::complex<double> const* x, st
   for (int i=0; i<n; i++)
      y[i] = a * x[i];
   return;
+}
+
+
+int domain_decomp(int global)
+{
+  int rank, size, local;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  if (global % size != 0)
+  {
+     std::cout << "To balance the workload among processors, it is recommended to choose a processor number that is divisible by A." << std::endl;
+     int td = std::round(double(global)/double(size));   
+     if (rank != size-1)
+        local = td;
+     else
+	local = global - td*rank;     
+  }
+  else
+  {
+     local = global / size;
+  }
+
+  return local;
 }
 
